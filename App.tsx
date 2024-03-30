f88f66b1a4bf8e25337ba7b6fdae0d542cdf4ed8
Gerald12344 <hxr286@student.bham.ac.uk>
2024-03-30 23:08:33 +0800

    stuff

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
+import { Text, View } from "react-native";
+import MainComp from "./app/components/main";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Sup Gamers</Text>
-      <StatusBar style="auto" />
+    <View>
+      <MainComp />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
